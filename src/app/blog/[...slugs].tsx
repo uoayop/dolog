b0ad0eda018cbcb21bb7d984ac83c56adb18b040
uoayop <uoayop@kakao.com>
2024-04-18 23:21:48 +0900

    chore: Prettier 적용

--- src/app/blog/[...slugs].tsx
+++ src/app/blog/[...slugs].tsx
@@ -2,29 +2,29 @@ import { GetStaticPaths, GetStaticProps } from "next";
 import { getAllPosts } from "../lib/posts/post";
 
 export const getStaticPaths: GetStaticPaths = () => {
-    const posts = getAllPosts();
+  const posts = getAllPosts();
 
-    return {
-        paths: posts.map((post) => post.slug),
-        fallback: 'blocking'
-    }
-}
+  return {
+    paths: posts.map((post) => post.slug),
+    fallback: "blocking",
+  };
+};
 
 export const getStaticProps: GetStaticProps = ({ params }) => {
-    const { slugs } = params as { slugs: string[] };
+  const { slugs } = params as { slugs: string[] };
 
-    const slug = `/posts/${slugs.join('/')}`;
-    const post = getAllPosts().find((post) => post.slug === slug);
+  const slug = `/posts/${slugs.join("/")}`;
+  const post = getAllPosts().find((post) => post.slug === slug);
 
-    if (!post) {
-        return {notFound: true}
-    }
+  if (!post) {
+    return { notFound: true };
+  }
 
-    return { props: { slug }}
-}
+  return { props: { slug } };
+};
 
-const PostPage = ({ slug}: {slug: string}) => {
-    return <div>{slug}</div>
+const PostPage = ({ slug }: { slug: string }) => {
+  return <div>{slug}</div>;
 };
 
 export default PostPage;
--- src/app/blog/index.tsx
+++ src/app/blog/index.tsx
@@ -1,24 +1,22 @@
-import { getAllPosts } from "../lib/posts/post"
+import { getAllPosts } from "../lib/posts/post";
 
 export const gteStaticProps = () => {
-    return {
-        props: {
-            posts: getAllPosts()
-        }
-    }
-}
+  return {
+    props: {
+      posts: getAllPosts(),
+    },
+  };
+};
 
-const PostListPage = ({ posts }: {posts: {slug: string}[]}) => {
-    return (
-        <ul>
-            {
-                posts.map((post, i) => {
-                    const keyofPost = `post-${i}`
-                    return (<li key={keyofPost}>{post.slug}</li>)
-                })
-            }
-        </ul>
-    )
-}
+const PostListPage = ({ posts }: { posts: { slug: string }[] }) => {
+  return (
+    <ul>
+      {posts.map((post, i) => {
+        const keyofPost = `post-${i}`;
+        return <li key={keyofPost}>{post.slug}</li>;
+      })}
+    </ul>
+  );
+};
 
 export default PostListPage;
